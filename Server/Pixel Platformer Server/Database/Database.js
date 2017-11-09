023226cd098475a6adece20e0adfee2620bdb20e
MatthewFrench <matthewf@ksu.edu>
2017-11-08 19:01:47 -0600

    Added configuration files and some starting structure files.

---
+++ Server/Pixel Platformer Server/Database/Database.js
@@ -0,0 +1,13 @@
+let mysql = require("mysql");
+
+//MySQL database initialization
+
+//Execute Query Function
+
+class Database {
+  constructor() {
+
+  }
+}
+
+exports.Database = Database;
---
+++ Server/Pixel Platformer Server/Networking/NetworkController.js
@@ -0,0 +1,1 @@
+//Socket.io code goes here, managed here
