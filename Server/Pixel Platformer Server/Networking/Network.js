07b2d80b93746cd3c83ed12d802e014a2d2e1d78
MatthewFrench <matthewf@ksu.edu>
2017-11-10 10:55:43 -0600

    Added websocket server stuff.

--- Server/Pixel Platformer Server/Networking/Network.js
+++ Server/Pixel Platformer Server/Networking/Network.js
@@ -1,2 +1,33 @@
 //Socket.io code goes here, managed here
 
+let socketIO = require('socket.io');
+let server = null;
+
+class Network {
+  static Initialize() {
+    server = socketIO(7777);
+    console.log('Websocket server is online!');
+
+    server.on('connection', function (socket) {
+      Network.ClientConnected(socket);
+      socket.on('message', function () {
+        Network.ClientMessage(socket);
+      });
+      socket.on('disconnect', function () {
+        Network.ClientDisconnected(socket);
+      });
+    });
+  }
+  static ClientConnected(socket) {
+    //Client connected
+    console.log('Client Connected!');
+  }
+  static ClientMessage(socket) {
+    console.log('Got message from client!');
+  }
+  static ClientDisconnected(socket) {
+    console.log('Client Disconnected!');
+  }
+}
+
+exports.Network = Network;
