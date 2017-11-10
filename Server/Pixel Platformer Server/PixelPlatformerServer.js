6d2c33a906d70f985190c97cbd1253cf42bfff0b
MatthewFrench <matthewf@ksu.edu>
2017-11-10 10:55:43 -0600

    Added websocket server stuff.

--- Server/Pixel Platformer Server/PixelPlatformerServer.js
+++ Server/Pixel Platformer Server/PixelPlatformerServer.js
@@ -1,11 +1,13 @@
 const {Query} = require("./Database/Query.js");
 const {Configuration} = require("./Configuration.js");
+const {Network} = require("./Networking/Network.js");
 
 class PixelPlatformerServer {
   constructor() {
     Configuration.Initialize(); //Load the config file
     Query.Initialize(() => {
       //Connected to the DB
+      Network.Initialize();
     });
   }
 }
