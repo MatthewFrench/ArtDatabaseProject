b406192c40fee3855376ef94808a1e40d6f8e98b
Ryan Bates <bates829@gmail.com>
2017-11-11 11:45:03 -0600

    Added sass loader

--- Game/webpack.config.js
+++ Game/webpack.config.js
@@ -7,9 +7,16 @@ module.exports = {
     filename: "./Build/bundle.js"
   },
   module: {
-    loaders: [
-      { test: /\.css$/, loader: "style!css" }
-    ]
+      rules: [{
+          test: /\.scss$/,
+          use: [{
+              loader: "style-loader" // creates style nodes from JS strings
+          }, {
+              loader: "css-loader" // translates CSS into CommonJS
+          }, {
+              loader: "sass-loader" // compiles Sass to CSS
+          }]
+      }]
   },
   plugins: [new HtmlWebpackPlugin()]
 };
