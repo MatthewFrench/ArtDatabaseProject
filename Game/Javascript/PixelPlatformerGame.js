88bbeab99dced8b2b6c66946c82d7781e24db4b3
MatthewFrench <matthewf@ksu.edu>
2017-11-11 12:41:17 -0600

    Attempt 2 at fixing untracked files.

--- Game/Javascript/PixelPlatformerGame.js
+++ Game/Javascript/PixelPlatformerGame.js
@@ -2,6 +2,6 @@
 
 export class PixelPlatformerGame {
   constructor() {
-    document.body.innerText = 'Pixel platformer game initialized.';
+    document.body.innerText = 'Pixel platformer game initialized. 2';
   }
 }
