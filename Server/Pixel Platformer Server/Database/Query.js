bf13b686b9b508733699921420bb834da1ab4da0
Devlin Smiley <dsmiles@Devlins-Mac.local>
2017-11-16 19:41:50 -0600

    Querys updated, still needing the user querys

--- Server/Pixel Platformer Server/Database/Query.js
+++ Server/Pixel Platformer Server/Database/Query.js
@@ -80,37 +80,75 @@ class Query {
     });
   }
 
+  /* Templates
+
+  static TITLE() {
+    //Get a connection
+    databaseInstance.getConnection((connection)=>{
+      //Create SQL
+      let sql = "";
+      //Execute Query
+      connection.query(sql, [], function (err, result) {
+        //Crash on error
+        if (err) console.log('Error: ' + err);
+        //Release the connection
+        connection.release();
+        //Pass back results
+        resultCallback(result);
+      });
+    });
+  }
+
+  */
+
+
+
   /****** TILE QUERIES ******/
   /**	GetAllTiles
   * Retrieves all "tile information."
   * (Params) - boardID
   * (Returns) - all “tile information” for the board
   */
-  
+    static getAllTiles(boardID) {
+      //Get a connection
+      databaseInstance.getConnection((connection)=>{
+        //Create SQL
+        let sql = "Select * from Tile where board_id = ? ";
+        //Execute Query
+        connection.query(sql, [boardID], function (err, result) {
+          //Crash on error
+          if (err) console.log('Error: ' + err);
+          //Release the connection
+          connection.release();
+          //Pass back results
+          resultCallback(result);
+        });
+      });
+    }
   
   /**	SetTile
   * After changing a tile in local memory, send the
   * update information to the board’s database.
-  *  (Params) - “tile info”
+  *  (Params) - tileID, boardID, x, y, color, creatorID, lastModifiedID
   * (Returns) - boolean
   */
-  static setTile(tileID, boardID, x, y, color, creatorID, lastModifiedID)
-  {
-   var sql = "insert into Tile (tile_id,board_id,x,y,color,creator_id, last_modified_id) values (tileID, boardID, x, y, color, creatorID, lastModifiedID) on duplicate key update  
-
-  static CreateAccount(username, hashedPassword) {
-    //let statement = mysql.createQuery("select * FROM Users where user=? and pass=?",
-    //  [username, hashedPassword]);
-    //Execute on database
-    //Get user
-    //Return user or null if none
-
-    var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-    databaseInstance.query(sql, [parameters], function (err, result) {
-      if (err) throw err;
-      console.log("1 record inserted");
-    });
-  }
+    static setTile(tileID, boardID, x, y, color, creatorID, lastModifiedID)
+    { 
+      //Get a connection
+      databaseInstance.getConnection((connection)=>{
+        //Create SQL  
+        let sql = "insert into Tile (tile_id,board_id,x,y,color,creator_id, last_modified_id) values (?,?,?,?,?,?,?) on duplicate key update color = ?";
+        //Execute Query
+        connection.query(sql, [tileID, boardID, x, y, color, creatorID, lastModifiedID, color], function (err, result) {
+          //Crash on error
+          if (err) console.log('Error: ' + err);
+          //Release the connection
+          connection.release();
+          //Pass back results
+          resultCallback(result);
+        });
+      });
+    }
 		/****** PLAYER QUERIES ******/
   /**
    * Get user information.
@@ -158,25 +196,73 @@ class Query {
 * (Returns) - boolean
 */
 	
-			/****** BOARD QUERIES ******/
+      /****** BOARD QUERIES ******/
+      
 /** 	CreateBoard
 * Creates a new board table in the database.
-* (Params) - boardName, playerID
+* (Params) - boardID, boardName, playerID
 * (Returns) - boolean
 */
 
+  static createBoard(boardID, boardName, playerID) {
+    //Get a connection
+    databaseInstance.getConnection((connection)=>{
+      //Create SQL
+      let sql = "insert into Board(board_id, name, creator_id) values (?,?,?)";
+      //Execute Query
+      connection.query(sql, [boardID, boardName, playerID], function (err, result) {
+        //Crash on error
+        if (err) console.log('Error: ' + err);
+        //Release the connection
+        connection.release();
+        //Pass back results
+        resultCallback(result);
+      });
+    });
+  }
 /**  	ChangeBoardName
 *  Change name of board.
-*  (Params)  boardID 
+*  (Params)  boardID, newName 
 *  (Return)  NULL
 */
+  static changeBoardName(boardID, newName) {
+    //Get a connection
+    databaseInstance.getConnection((connection)=>{
+      //Create SQL
+      let sql = "update Board set name = ? where board_id = ?";
+      //Execute Query
+      connection.query(sql, [newName, boardID], function (err, result) {
+        //Crash on error
+        if (err) console.log('Error: ' + err);
+        //Release the connection
+        connection.release();
+        //Pass back results
+        resultCallback(result);
+      });
+    });
+  }
 
 /**  	RemoveBoard
 *  Remove entry for matching board ID.
-*  (Params) - BoardID, playerID
+*  (Params) - BoardID
 *  (Returns) - NULL
 */
-
+  static removeBoard(boardId) {
+    //Get a connection
+    databaseInstance.getConnection((connection)=>{
+      //Create SQL
+      let sql = "update Board set is_deleted = 1 where board_id = ?";
+      //Execute Query
+      connection.query(sql, [board_id], function (err, result) {
+        //Crash on error
+        if (err) console.log('Error: ' + err);
+        //Release the connection
+        connection.release();
+        //Pass back results
+        resultCallback(result);
+      });
+    });
+  }
 			/****** SPRITE QUERIES ******/
 /**		GetAllSprites
 * Pull all sprite information to local memory for later loading.
