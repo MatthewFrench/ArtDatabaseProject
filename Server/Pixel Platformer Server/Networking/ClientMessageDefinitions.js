01a42740d7498134d002b597a134944dac4fddc7
MatthewFrench <matthewf@ksu.edu>
2017-11-16 20:59:27 -0600

    Added some of the network messaging framework.

---
+++ Server/Pixel Platformer Server/Networking/ClientMessageDefinitions.js
@@ -0,0 +1,28 @@
+//These are the definitions that the client receives.
+
+const Controllers = {
+  Login: {
+    ID: 1,
+    Messages: {
+      LoginSuccess: 1
+    }
+  },
+  Register: {
+    ID: 2,
+    Messages: {
+      RegisterSuccess: 1
+    }
+  },
+  Chat: {
+    ID: 3,
+    Messages: {
+    }
+  },
+  Game: {
+    ID: 4,
+    Messages: {
+    }
+  }
+};
+
+exports = Controllers;
