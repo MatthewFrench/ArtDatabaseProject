023da45821722885923638df1061e80a1078fb30
Ryan Bates <bates829@gmail.com>
2017-11-17 10:04:43 -0600

    Created game page

---
+++ Game/Javascript/Pages/Game/Game.js
@@ -0,0 +1,22 @@
+import {Interface} from "../../Utility/Interface";
+
+export class Game{
+    constructor(switchToLoginPage){
+        this.mainDiv = Interface.Create({type:'div', className: 'GamePage', elements:[
+            {type: 'div', elements: [
+                {type: 'div', className: 'worldWrapper', elements: [
+                    {type: 'canvas', className: 'gameArea'},
+                    {type: 'div', className: 'worldSelect'}
+                ]},
+                {type: 'div', className: 'chatArea', elements: [
+                    {type: 'textarea', className: 'messageLog'},
+                    {type: 'input', inputType: 'text', className: 'textInput', placeholder: 'Enter your message...'}
+                ]},
+                {type: 'div', text: 'Logout', className: 'logoutBtn', onClick: () => {switchToLoginPage();}},
+            ]}
+        ]});
+    }
+    getDiv = () => {
+        return this.mainDiv;
+    }
+}
--- Game/Javascript/Pages/Login.js
+++ Game/Javascript/Pages/Login.js
@@ -4,7 +4,7 @@ import {Interface} from "../Utility/Interface.js";
 import {ScorePopover} from "./Game/ScorePopover.js";
 
 export class Login {
-    constructor(switchToRegisterPage) {
+    constructor(switchToRegisterPage, switchToGamePage) {
         this.mainDiv = Interface.Create({type: 'div', className: 'LoginPage', elements: [
           {type: 'div', elements: [
             {type: 'h2', text: 'Login to Pixel Platformer', className: 'loginText'},
@@ -12,7 +12,7 @@ export class Login {
             {type: 'p'},
             {type: 'input', inputType: 'password', className: 'passw', placeholder: 'Password'},
             {type: 'p'},
-            {type: 'div', text: "Login", className: 'loginBtn'},
+            {type: 'div', text: "Login", className: 'loginBtn', onClick: () => {switchToGamePage();}},
             {type: 'p'},
             {type: 'div', text: "Need an account? Register here:", className: 'loginText'},
             {type: 'div', text:'Register', className: 'registerBtn', onClick: () => {switchToRegisterPage();}},
