65a9a662c59225e199b0459fb96d2fbc43eb6e86
MatthewFrench <matthewf@ksu.edu>
2017-11-23 12:51:10 -0600

    Working on the code

--- Game/Javascript/Pages/Game/Renderer/TileLayer/FragmentShader.glsl
+++ Game/Javascript/Pages/Game/Renderer/TileLayer/FragmentShader.glsl
@@ -1,9 +1,5 @@
 precision mediump float;
-varying vec4 param;
+//varying vec4 param;
 void main() {
-    float border = step(param.z, param.x) *
-                   step(param.w, param.y) *
-                   step(param.z, 1.0 - param.x) *
-                   step(param.w, 1.0 - param.y);
-    gl_FragColor = vec4(border * vec3(1,1,1), 1);
+    gl_FragColor = vec4(1, 0, 0.5, 1); // return redish-purple
 }
