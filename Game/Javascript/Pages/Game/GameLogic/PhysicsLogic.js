a2d886c90367db4b7afd242c223a55a0af2a207b
MatthewFrench <matthewf@ksu.edu>
2017-11-25 02:12:18 -0600

    Wrote custom physics engine. Very basic.

--- Game/Javascript/Pages/Game/GameLogic/PhysicsLogic.js
+++ Game/Javascript/Pages/Game/GameLogic/PhysicsLogic.js
@@ -1,150 +1,182 @@
-const b2 = require('lucy-b2');
-
-let Tiles_Per_Meter = 3.8; //3.8 tiles per meter
-let Player_Tile_Width = 2;
-let Player_Tile_Height = 5;
+const Player_Width_Tiles = 2;
+//Player height is 5 but subtract a tiny bit to allow physics to pass it underneath tight spaces.
+const Player_Height_Tiles = 4.9;
+const Gravity = 0.01;
+const Player_Move_Speed = 0.01;
+const Player_Jump_Speed = 0.4;
+const Ground_Friction = 0.98;
+const Cut_Off = 0.0001;
 
 export class PhysicsLogic {
     constructor() {
-        //Holds tiles in format Tile -> Body
-        this.tileBodies = new Map();
-        //Holds players in format Player -> Body
-        this.playerBodies = new Map();
-
-        let gravity = new b2.Vec2(0, -9.8);
-        this.world = new b2.World(gravity);
     }
+    logic = (board) => {
+        this.unhighlightAllTiles(board);
+        let players = board.getPlayers();
+        //Loop through all players
+        players.forEach((player)=>{
+            //Add gravity and friction
+            player.speedY -= Gravity;
+            player.speedX *= Ground_Friction;
+            //Cut off horizontal speed after a point
+            if (Math.abs(player.speedX) <= Cut_Off) {
+                player.speedX = 0;
+            }
 
-    addTileBody(tile) {
-        // Define body
-        let bodyDef = new b2.BodyDef();
-        bodyDef.type = b2.BodyType.staticBody;
+            //Check for collision
+            let left = Math.floor(player.x - Player_Width_Tiles/2 + 0.5);
+            let right = Math.floor(player.x + Player_Width_Tiles/2 + 0.5);
+            let bottom = Math.floor(player.y);
+            let top = Math.floor(player.y + Player_Height_Tiles);
 
-        // Create body with definition
-        let body = this.world.CreateBody(bodyDef);
+            let onGround = this.isHorizontalCollision(board, left, right, bottom);
+            let bottomNextStep = Math.floor(player. y + player.speedY);
+            let nextStepOnGround = this.isHorizontalCollision(board, left, right, bottomNextStep);
 
-        // Define fixture
-        let fixDef = new b2.FixtureDef();
-        fixDef.density = 1.0;
-        fixDef.friction = 0.2;
-        fixDef.restitution = 0.0;
+            //Check if on ground
+            if ((onGround || nextStepOnGround) && player.speedY < 0) {
+                player.speedY = 0;
+                //Set the tile to be above the ground tile
+                if (onGround) {
+                    this.highlightTiles(this.getTilesInHorizontalCollision(board, left, right, bottom));
+                    player.y = bottom + 1;
+                } else {
+                    this.highlightTiles(this.getTilesInHorizontalCollision(board, left, right, bottomNextStep));
+                    player.y = bottomNextStep + 1;
+                }
+                bottom = Math.floor(player.y);
+                top = Math.floor(player.y + Player_Height_Tiles);
+            }
+            onGround = onGround || nextStepOnGround;
 
-        let shape = new b2.PolygonShape();
-        shape.Set([new b2.Vec2(0, 0),
-            new b2.Vec2(1 / Tiles_Per_Meter, 0),
-            new b2.Vec2(1 / Tiles_Per_Meter, 1 / Tiles_Per_Meter),
-            new b2.Vec2(0, 1 / Tiles_Per_Meter)]);
-        //shape.SetAsBox(1 / Tiles_Per_Meter, 1 / Tiles_Per_Meter);
+            //Check to see if we're hitting a tile from above
+            let topNextStep = Math.floor(player.y + Player_Height_Tiles + player.speedY);
+            let topSideHit = this.isHorizontalCollision(board, left, right, top);
+            let nextStepTopSideHit = this.isHorizontalCollision(board, left, right, topNextStep);
+            if ((topSideHit || nextStepTopSideHit) && player.speedY > 0) {
+                player.speedY = 0;
+                if (topSideHit) {
+                    this.highlightTiles(this.getTilesInHorizontalCollision(board, left, right, top));
+                    player.y = top - Player_Height_Tiles;
+                } else {
+                    this.highlightTiles(this.getTilesInHorizontalCollision(board, left, right, topNextStep));
+                    player.y = topNextStep - Player_Height_Tiles;
+                }
 
-        fixDef.shape = shape;
+                bottom = Math.floor(player.y);
+                top = Math.floor(player.y + Player_Height_Tiles);
+            }
 
-        // Create fixture
-        body.CreateFixture(fixDef);
+            //Apply movement
+            if (player.movingLeft) {
+                player.speedX -= Player_Move_Speed;
+            }
+            if (player.movingRight) {
+                player.speedX += Player_Move_Speed;
+            }
+            if (player.jumping && onGround && player.speedY < Player_Jump_Speed) {
+                player.speedY += Player_Jump_Speed;
+            }
 
-        // Move body into initial position ( and rotation )
-        body.SetTransform(tile.getX() / Tiles_Per_Meter, tile.getY() / Tiles_Per_Meter, 0);
-
-        this.tileBodies.set(tile, body);
-    }
-
-    addPlayerBody(player) {
-        // Define body
-        let bodyDef = new b2.BodyDef();
-        bodyDef.type = b2.BodyType.dynamicBody;
-
-        // Create body with definition
-        let body = this.world.CreateBody(bodyDef);
-
-        // Define fixture
-        let fixDef = new b2.FixtureDef();
-        fixDef.density = 1.0;
-        fixDef.friction = 0.2;
-        fixDef.restitution = 0.0;
-
-        let shape = new b2.PolygonShape();
-        let playerWidth = Player_Tile_Width / Tiles_Per_Meter;
-        shape.Set([new b2.Vec2(-playerWidth/2, 0),
-            new b2.Vec2(playerWidth-playerWidth/2, 0),
-            new b2.Vec2(playerWidth-playerWidth/2, Player_Tile_Height / Tiles_Per_Meter),
-            new b2.Vec2(-playerWidth/2, Player_Tile_Height / Tiles_Per_Meter)]);
-        //shape.SetAsBox(Player_Tile_Width / Tiles_Per_Meter, Player_Tile_Height / Tiles_Per_Meter);
-
-        fixDef.shape = shape;
-
-        // Create fixture
-        body.CreateFixture(fixDef);
-
-        // Move body into initial position ( and rotation )
-        body.SetTransform(player.getX() / Tiles_Per_Meter, player.getY() / Tiles_Per_Meter, 0);
-
-        this.playerBodies.set(player, body);
-    }
-
-    getTileBody = (tile) => {
-        let body = this.tileBodies.get(tile);
-        if (body === undefined) {
-            return null;
-        }
-        return body;
-    };
-
-    getPlayerBody = (player) => {
-        let body = this.playerBodies.get(player);
-        if (body === undefined) {
-            return null;
-        }
-        return body;
-    };
-
-    updateTileBodyPosition = (tile, x, y) => {
-        let body = this.getTileBody(tile);
-        if (body !== null) {
-            body.SetTransform(x / Tiles_Per_Meter, y / Tiles_Per_Meter, 0);
-        } else {
-            //Create tile
-            this.addTileBody(tile);
-        }
-    };
-
-    removeTileBody = (tile) => {
-        let body = this.getTileBody(tile);
-        if (body !== null) {
-            this.world.DestroyBody(body);
-            this.tileBodies.delete(tile);
-        }
-    };
-
-    updatePlayerBodyPosition = (player, x, y) => {
-        let body = this.getPlayerBody(player);
-        if (body !== null) {
-            body.SetTransform(x / Tiles_Per_Meter, y / Tiles_Per_Meter, 0);
-        }
-    };
-
-    updatePlayerPositionFromBody = (player) => {
-        let playerBody = this.getPlayerBody(player);
-        if (playerBody !== null) {
-            player.setX(playerBody.GetPosition().x);
-            player.setY(playerBody.GetPosition().y);
-        }
-    };
-
-    applyForceToPlayer = (player, xForce, yForce) => {
-        let playerBody = this.getPlayerBody(player);
-        if (playerBody !== null) {
-            playerBody.ApplyForceToCenter(new b2.Vec2(xForce, yForce), true);
-        }
-    };
-
-    logic = () => {
-        /*
-        this.world.Step(1 / 60, 30, 30);
-
-        //Update player locations with physics locations
-        this.playerBodies.forEach((body, player)=>{
-            player.setX(body.GetPosition().x);
-            player.setY(body.GetPosition().y);
+            //Do side collisions
+            let leftNextStep = Math.floor(player.x - Player_Width_Tiles/2 + 0.5 + player.speedX);
+            let leftSideHit = this.isVerticalCollision(board, left, bottom, top);
+            //Ignore the first step so it can climb stairs automatically (bottom + 1)
+            let nextStepLeftSideHit = this.isVerticalCollision(board, leftNextStep, bottom + 1, top);
+            if ((leftSideHit || nextStepLeftSideHit) && player.speedX < 0) {
+                player.speedX = 0;
+                if (leftSideHit) {
+                    this.highlightTiles(this.getTilesInVerticalCollision(board, left, bottom, top));
+                    player.x = left + 1 + 0.5;
+                } else {
+                    this.highlightTiles(this.getTilesInVerticalCollision(board, leftNextStep, bottom, top));
+                    player.x = leftNextStep + 1 + 0.5;
+                }
+            }
+            //Check right side collisions
+            let rightNextStep = Math.floor(player.x + Player_Width_Tiles/2 + 0.5 + player.speedX);
+            let rightSideHit = this.isVerticalCollision(board, right, bottom, top);
+            //Ignore the first step so it can climb stairs automatically (bottom + 1)
+            let nextStepRightSideHit = this.isVerticalCollision(board, rightNextStep, bottom + 1, top);
+            if ((rightSideHit || nextStepRightSideHit) && player.speedX > 0) {
+                player.speedX = 0;
+                if (rightSideHit) {
+                    this.highlightTiles(this.getTilesInVerticalCollision(board, right, bottom, top));
+                    player.x = right - 1 - 0.5;
+                } else {
+                    this.highlightTiles(this.getTilesInVerticalCollision(board, rightNextStep, bottom, top));
+                    player.x = rightNextStep - 1 - 0.5;
+                }
+            }
+            //Add player speed to position
+            player.x += player.speedX;
+            player.y += player.speedY;
         });
-        */
-    }
+    };
+
+    unhighlightAllTiles = (board) => {
+        board.tiles.forEach((x) => {
+            x.forEach((tile)=>{
+                if (tile.a > 0) {
+                    tile.r = 0;
+                    tile.g = 1;
+                    tile.b = 0;
+                    tile.a = 1;
+                }
+            });
+        });
+    };
+
+    highlightTiles = (tiles) => {
+        for (let tile of tiles) {
+            tile.r = 1;
+            tile.g = 0;
+            tile.b = 0;
+            tile.a = 1;
+        }
+    };
+
+    getTilesInHorizontalCollision = (board, x1, x2, y) => {
+        let tiles = [];
+        for (let x = x1; x <= x2; x++) {
+            let tile = board.getTile(x, y);
+            if (tile !== null && tile.a !== 0) {
+                tiles.push(tile);
+            }
+        }
+        return tiles;
+    };
+
+    isHorizontalCollision = (board, x1, x2, y) => {
+        for (let x = x1; x <= x2; x++) {
+            let tile = board.getTile(x, y);
+            if (tile !== null && tile.a !== 0) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+
+    getTilesInVerticalCollision = (board, x, y1, y2) => {
+        let tiles = [];
+        for (let y = y1; y <= y2; y++) {
+            let tile = board.getTile(x, y);
+            if (tile !== null && tile.a !== 0) {
+                tiles.push(tile);
+            }
+        }
+        return tiles;
+    };
+
+    isVerticalCollision = (board, x, y1, y2) => {
+        for (let y = y1; y <= y2; y++) {
+            let tile = board.getTile(x, y);
+            if (tile !== null && tile.a !== 0) {
+                return true;
+            }
+        }
+        return false;
+    };
+
 }
