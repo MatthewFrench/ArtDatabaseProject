531008a57810fef299db6cce9664ad34f1d79d64
MatthewFrench <matthewf@ksu.edu>
2017-11-26 22:30:49 -0600

    Webpack config changes

--- Game/webpack.config.js
+++ Game/webpack.config.js
@@ -2,54 +2,55 @@ let HtmlWebpackPlugin = require('html-webpack-plugin');
 let webpack = require('webpack');
 
 module.exports = {
+    context: __dirname,
   entry: "./main.js",
   output: {
     path: __dirname + "/Build/",
     filename: "./bundle.js"
   },
-  module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [{
-        loader: "style-loader" // creates style nodes from JS strings
-      }, {
-        loader: "css-loader" // translates CSS into CommonJS
-      }, {
-        loader: "sass-loader" // compiles Sass to CSS
-      }]
-    },
-
-        {
-            test: /\.(jpg|png|svg)$/,
-            use: [
-                {
-                    loader: 'file-loader',
-                    options: {}
-                }
-            ]
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
         },
+            {
+                test: /\.(jpg|png|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
 
-
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['es2015'],
-            plugins: ['transform-class-properties']
-          }
-        }
-      }, {
-            test: /\.glsl$/,
-            loader: 'webpack-glsl-loader'
-        }]
-  },
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015'],
+                        plugins: ['transform-class-properties']
+                    }
+                }
+            },
+            {
+                test: /\.glsl$/,
+                loader: 'webpack-glsl-loader'
+            }]
+    },
   plugins: [
     new HtmlWebpackPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"production"'
     }),
+      /*
     new webpack.optimize.UglifyJsPlugin({
       mangle: true,
       compress: {
@@ -64,6 +65,7 @@ module.exports = {
       },
       exclude: [/\.min\.js$/gi] // skip pre-minified libs
     }),
+    */
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
     new webpack.NoEmitOnErrorsPlugin(),
