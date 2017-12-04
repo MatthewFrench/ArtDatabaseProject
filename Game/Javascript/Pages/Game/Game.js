654278b772cde7b37f307502b64d32b1f38b0489
tjviles <tjviles@gmail.com>
2017-12-04 09:38:27 -0600

    Added BG music and slider, added some sprite select functionality

--- Game/Javascript/Pages/Game/Game.js
+++ Game/Javascript/Pages/Game/Game.js
@@ -27,6 +27,7 @@ export class Game{
                         this.gameLogic.getPreviewSquare(),
                         this.toggleSelector = Interface.Create({type: 'div', className: 'ToggleSelector', text: 'Minimize', onClick: this.slideToggle}),
                     ]}),
+                    this.gameLogic.getVolumeSlider(),
                     this.gameLogic.getCanvas(),
                     (this.boardSelector = new BoardSelector(this)).getDiv()
 
@@ -137,7 +138,7 @@ export class Game{
 
     spriteButtonClicked = () =>{
         this.mainDiv.appendChild(this.spritePopover.getDiv());
-    }
+    };
 
     getDiv = () => {
         return this.mainDiv;
