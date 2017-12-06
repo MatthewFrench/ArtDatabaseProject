21710d6de19fe9e6b8f643a7deb2e7f2633448b4
MatthewFrench <matthewf@ksu.edu>
2017-12-06 14:31:12 -0600

    Ton of physics improvements

--- Server/Pixel Platformer Server/Logic/Game/Physics.ts
+++ Server/Pixel Platformer Server/Logic/Game/Physics.ts
@@ -1,4 +1,5 @@
 import {Board} from "./Board";
+import {Player} from "../../Player/Player";
 
 const Player_Width_Tiles = 2;
 //Player height is 5 but subtract a tiny bit to allow physics to pass it underneath tight spaces.
@@ -10,136 +11,160 @@ const Ground_Friction = 0.98;
 const Cut_Off = 0.0001;
 
 const Solid_Tile_Type = 4;
+const Tunnel_Speed_Cap = 0.5;
 
 export class Physics {
     board: Board;
+    runningPhysics = 0;
     constructor(board) {this.board = board;}
     logic = (delta) => {
-        while (delta >= 1.75) {
+        while (delta > 1.0) {
             this.runPhysics(1.0);
             delta -= 1.0;
         }
         this.runPhysics(delta);
     };
 
-    runPhysics = (delta) => {
+    runPhysics = (physicsDelta) => {
         let players = this.board.getPlayers();
         //Loop through all players
         players.forEach((player)=>{
             let playerGameData = player.getGameData();
-            let speedX = playerGameData.getSpeedX();
-            let speedY = playerGameData.getSpeedY();
-            let x = playerGameData.getX();
-            let y = playerGameData.getY();
-            let movingLeft = playerGameData.getMovingLeft();
-            let movingRight = playerGameData.getMovingRight();
-            let jumping = playerGameData.getJumping();
-            //Add gravity and friction
-            speedY -= Gravity * delta;
-            //Cut off horizontal speed after a point
-            if (Math.abs(speedX) <= Cut_Off) {
-                speedX = 0;
-            }
-
-            //Check for collision
-            let left = Math.floor(x - Player_Width_Tiles/2 + 0.5);
-            let right = Math.floor(x + Player_Width_Tiles/2);
-            let bottom = Math.floor(y);
-            let top = Math.floor(y + Player_Height_Tiles);
-
-            let onGround = this.isHorizontalCollision(left, right, bottom);
-            let bottomNextStep = Math.floor( y + speedY);
-            let nextStepOnGround = this.isHorizontalCollision(left, right, bottomNextStep);
-
-            //Check if on ground
-            if ((onGround || nextStepOnGround) && speedY < 0) {
-                speedY = 0;
-                //Set the tile to be above the ground tile
-                if (onGround) {
-                    y = bottom + 1;
+            let totalDelta = 0;
+            while (totalDelta < physicsDelta) {
+                let deltaLeft = physicsDelta - totalDelta;
+                let speedX = playerGameData.getSpeedX();
+                let speedY = playerGameData.getSpeedY();
+                let playerSpeed = Math.hypot(speedX, speedY);
+                if (playerSpeed * deltaLeft > Tunnel_Speed_Cap) {
+                    let maxAllowedDelta = Tunnel_Speed_Cap / playerSpeed;
+                    //Calculate the maximum safe delta for anti-tunnelling
+                    this.runPlayerPhysicsLogic(player, maxAllowedDelta);
+                    totalDelta += maxAllowedDelta;
                 } else {
-                    y = bottomNextStep + 1;
-                }
-                bottom = Math.floor(y);
-                top = Math.floor(y + Player_Height_Tiles);
-            }
-            onGround = onGround || nextStepOnGround;
-
-            //Check to see if we're hitting a tile from above
-            let topNextStep = Math.floor(y + Player_Height_Tiles + speedY);
-            let topSideHit = this.isHorizontalCollision(left, right, top);
-            let nextStepTopSideHit = this.isHorizontalCollision(left, right, topNextStep);
-            if ((topSideHit || nextStepTopSideHit) && speedY > 0) {
-                speedY = 0;
-                if (topSideHit) {
-                    y = top - Player_Height_Tiles;
-                } else {
-                    y = topNextStep - Player_Height_Tiles;
-                }
-
-                bottom = Math.floor(y);
-                top = Math.floor(y + Player_Height_Tiles);
-            }
-
-            //Apply ground friction
-            if (onGround) {
-                //Is this correct? Do calculations to make sure
-                speedX *= Ground_Friction * delta;
-            }
-
-            //Apply movement
-            if (movingLeft) {
-                //Apply a massive slowdown to allow easy mid-air moving
-                if (speedX > 0) {
-                    speedX *= 0.9;
-                }
-                speedX -= Player_Move_Speed * delta;
-            }
-            if (movingRight) {
-                //Apply a massive slowdown to allow easy mid-air moving
-                if (speedX < 0) {
-                    speedX *= 0.9;
-                }
-                speedX += Player_Move_Speed * delta;
-            }
-            if (jumping && onGround && speedY < Player_Jump_Speed) {
-                speedY += Player_Jump_Speed;
-            }
-
-            //Do side collisions
-            let leftNextStep = Math.floor(x - Player_Width_Tiles/2 + 0.5 + speedX);
-            let leftSideHit = this.isVerticalCollision(left, bottom, top);
-            //Ignore the first step so it can climb stairs automatically (bottom + 1)
-            let nextStepLeftSideHit = this.isVerticalCollision(leftNextStep, bottom + 1, top);
-            if ((leftSideHit || nextStepLeftSideHit) && speedX < 0) {
-                speedX = 0;
-                if (leftSideHit) {
-                    x = left + 1 + 0.5;
-                } else {
-                    x = leftNextStep + 1 + 0.5;
+                    this.runPlayerPhysicsLogic(player, deltaLeft);
+                    totalDelta += deltaLeft;
                 }
             }
-            //Check right side collisions
-            let rightNextStep = Math.floor(x + Player_Width_Tiles/2 + 0.5 + speedX);
-            let rightSideHit = this.isVerticalCollision(right, bottom, top);
-            //Ignore the first step so it can climb stairs automatically (bottom + 1)
-            let nextStepRightSideHit = this.isVerticalCollision(rightNextStep, bottom + 1, top);
-            if ((rightSideHit || nextStepRightSideHit) && speedX > 0) {
-                speedX = 0;
-                if (rightSideHit) {
-                    x = right - 1 - 0.5;
-                } else {
-                    x = rightNextStep - 1 - 0.5;
-                }
-            }
-            //Add player speed to position
-            x += speedX * delta;
-            y += speedY * delta;
-            playerGameData.setX(x);
-            playerGameData.setY(y);
-            playerGameData.setSpeedX(speedX);
-            playerGameData.setSpeedY(speedY);
         });
+    };
+
+    runPlayerPhysicsLogic = (player: Player, delta: number)=> {
+        let playerGameData = player.getGameData();
+        let speedX = playerGameData.getSpeedX();
+        let speedY = playerGameData.getSpeedY();
+        let x = playerGameData.getX();
+        let y = playerGameData.getY();
+        let movingLeft = playerGameData.getMovingLeft();
+        let movingRight = playerGameData.getMovingRight();
+        let jumping = playerGameData.getJumping();
+
+        //Add gravity and friction
+        speedY -= Gravity * delta;
+
+        //Cut off horizontal speed after a point
+        if (Math.abs(speedX) <= Cut_Off) {
+            speedX = 0;
+        }
+
+        //Check for collision
+        let left = Math.floor(x - Player_Width_Tiles/2 + 0.5);
+        let right = Math.floor(x + Player_Width_Tiles/2);
+        let bottom = Math.floor(y);
+        let top = Math.floor(y + Player_Height_Tiles);
+
+        let onGround = this.isHorizontalCollision(left, right, bottom);
+        let bottomNextStep = Math.floor( y + speedY * delta);
+        let nextStepOnGround = this.isHorizontalCollision(left, right, bottomNextStep);
+
+        //Check if on ground
+        if ((onGround || nextStepOnGround) && speedY < 0) {
+            speedY = 0;
+            //Set the tile to be above the ground tile
+            if (onGround) {
+                y = bottom + 1;
+            } else {
+                y = bottomNextStep + 1;
+            }
+            bottom = Math.floor(y);
+            top = Math.floor(y + Player_Height_Tiles);
+        }
+        onGround = onGround || nextStepOnGround;
+
+        //Check to see if we're hitting a tile from above
+        let topNextStep = Math.floor(y + Player_Height_Tiles + speedY * delta);
+        let topSideHit = this.isHorizontalCollision(left, right, top);
+        let nextStepTopSideHit = this.isHorizontalCollision(left, right, topNextStep);
+        if ((topSideHit || nextStepTopSideHit) && speedY > 0) {
+            speedY = 0;
+            if (topSideHit) {
+                y = top - Player_Height_Tiles;
+            } else {
+                y = topNextStep - Player_Height_Tiles;
+            }
+
+            bottom = Math.floor(y);
+            top = Math.floor(y + Player_Height_Tiles);
+        }
+
+        //Apply ground friction
+        if (onGround) {
+            speedX *= Math.pow(Ground_Friction, delta);
+        }
+
+        //Apply movement
+        if (movingLeft) {
+            //Apply a massive slowdown to allow easy mid-air moving
+            if (speedX > 0) {
+                speedX *= Math.pow(0.9, delta);
+            }
+            speedX -= Player_Move_Speed * delta;
+        }
+        if (movingRight) {
+            //Apply a massive slowdown to allow easy mid-air moving
+            if (speedX < 0) {
+                speedX *= Math.pow(0.9, delta);
+            }
+            speedX += Player_Move_Speed * delta;
+        }
+        if (jumping && onGround && speedY < Player_Jump_Speed) {
+            //This is an impulse, instant. Delta does not get applied.
+            speedY += Player_Jump_Speed;
+        }
+
+        //Do side collisions
+        let leftNextStep = Math.floor(x - Player_Width_Tiles/2 + 0.5 + speedX * delta);
+        let leftSideHit = this.isVerticalCollision(left, bottom, top);
+        //Ignore the first step so it can climb stairs automatically (bottom + 1)
+        let nextStepLeftSideHit = this.isVerticalCollision(leftNextStep, bottom + 1, top);
+        if ((leftSideHit || nextStepLeftSideHit) && speedX < 0) {
+            speedX = 0;
+            if (leftSideHit) {
+                x = left + 1 + 0.5;
+            } else {
+                x = leftNextStep + 1 + 0.5;
+            }
+        }
+        //Check right side collisions
+        let rightNextStep = Math.floor(x + Player_Width_Tiles/2 + 0.5 + speedX * delta);
+        let rightSideHit = this.isVerticalCollision(right, bottom, top);
+        //Ignore the first step so it can climb stairs automatically (bottom + 1)
+        let nextStepRightSideHit = this.isVerticalCollision(rightNextStep, bottom + 1, top);
+        if ((rightSideHit || nextStepRightSideHit) && speedX > 0) {
+            speedX = 0;
+            if (rightSideHit) {
+                x = right - 1 - 0.5;
+            } else {
+                x = rightNextStep - 1 - 0.5;
+            }
+        }
+        //Add player speed to position
+        x += speedX * delta;
+        y += speedY * delta;
+        playerGameData.setX(x);
+        playerGameData.setY(y);
+        playerGameData.setSpeedX(speedX);
+        playerGameData.setSpeedY(speedY);
     };
 
     getTilesInHorizontalCollision = (x1, x2, y) => {
