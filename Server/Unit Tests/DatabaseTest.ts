4fab1eb1b58ba74dd94ed541f2fa08b2ea50595c
Ryan Huse <rmhks95@ksu.edu>
2018-05-01 16:52:12 -0500

    fixing up premade tests

--- Server/Unit Tests/DatabaseTest.ts
+++ Server/Unit Tests/DatabaseTest.ts
@@ -1,3 +1,5 @@
+import {Board} from "../Pixel Platformer Server/Logic/Game/Board";
+
 const assert = require('assert');
 import {Query} from "../Pixel Platformer Server/Database/Query";
 import {Configuration} from "../Pixel Platformer Server/Configuration";
@@ -9,15 +11,17 @@ export class DatabaseTest {
         console.log('Running Database Tests');
         Configuration.Initialize(); //Load the config file
         Query.Initialize();
-        this.createBoardFail();
-        var curBoard;
+        //this.createBoard();
+        //this.getAllTilesTest();
+        this.getBoardInfoTest(); //passes
+        this.getSpriteTest(); //passes
+        //this.insertTileTest();
         //this.testAllDataValues().then();
     }
 
 
     async testAllDataValues() {
         console.log('Testing Database Queries');
-
 
 
         await this.getSpriteTest();
@@ -33,11 +37,11 @@ export class DatabaseTest {
         console.log('\nDatabase Test Success');
     }
 
-    async createBoardFail(){
+    async createBoard(){
 
         var info = await Query.CreateBoard("hello", "hahahahahaahaahhaahaeiordwfjakf");
 
-        assert.equal(info, 38, "Did not pass")
+        assert.equal(info, 42, "Did not pass")
 
 
     }
@@ -53,8 +57,11 @@ export class DatabaseTest {
         let milliseconds = (difference[0] + difference[1] / NS_PER_SEC) * 1000;
         console.log('Sprite Test Duration(ms): ' + milliseconds);
 
-        //Test print out sprites
-        console.log("Sprites: " + JSON.stringify(spriteResults));
+        var correct = JSON.stringify([{"sprite_id":1,"image_url":"M_TOPHAT_BROWN"},{"sprite_id":2,"image_url":"M_TOPHAT_BLACK"},{"sprite_id":3,"image_url":"M_NEWSBOY"},{"sprite_id":4,"image_url":"M_DOCTOR_MASK"},{"sprite_id":5,"image_url":"M_DOCTOR_NOMASK"},{"sprite_id":6,"image_url":"F_NURSE"},{"sprite_id":7,"image_url":"M_DOCTOR_BLOODY"},{"sprite_id":8,"image_url":"M_TOPHAT_GLASSES"}]);
+
+
+        assert.equal(JSON.stringify(spriteResults),correct, "Sprites didn't equal")
+        console.log("getSpriteTest passed")
     }
 
     async getAllTilesTest() {
@@ -98,7 +105,18 @@ export class DatabaseTest {
         let milliseconds = (difference[0] + difference[1] / NS_PER_SEC) * 1000;
         console.log('Tiles Test Duration(ms): ' + milliseconds);
 
-        //Test print out sprites
-        console.log("Board: " + JSON.stringify(results));
+
+        var info = results;
+
+        assert.equal(info.board_id, 1, "Board ID is wrong")
+        assert.equal(info.name, "Home", "Board Name is wrong")
+        assert.equal(info.creator_id, 5, "Creator ID is wrong")
+        assert.equal(info.is_deleted, 0, "Board is not deleted")
+        assert.equal(info.max_width,-1,"Max width should be -1")
+        assert.equal(info.max_height, -1, "Max height should be -1")
+        console.log("getBoardInfoTest Passed");
+
+
+
     }
 }
