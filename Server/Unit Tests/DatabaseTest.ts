09f67e83fd8f489ba8f06b49766e974d3217b66c
Ryan Huse <rmhks95@ksu.edu>
2018-05-03 13:18:21 -0500

    fixing up tests

--- Server/Unit Tests/DatabaseTest.ts
+++ Server/Unit Tests/DatabaseTest.ts
@@ -11,11 +11,12 @@ export class DatabaseTest {
         console.log('Running Database Tests');
         Configuration.Initialize(); //Load the config file
         Query.Initialize();
-        //this.createBoard();
+        this.createBoardTest(); // passes
+        this.userLoginTest();
         //this.getAllTilesTest();
-        //this.getBoardInfoTest(); //passes
-        //this.getSpriteTest(); //passes
-        //this.insertTileTest(); //passes
+        this.getBoardInfoTest(); //passes
+        this.getSpriteTest(); //passes
+        this.insertTileTest(); //passes
         //this.testAllDataValues().then();
     }
 
@@ -37,24 +38,35 @@ export class DatabaseTest {
         console.log('\nDatabase Test Success');
     }
 
-    async createBoard(){
+    async createBoardTest(){
 
         var info = await Query.CreateBoard("hello", "hahahahahaahaahhaahaeiordwfjakf");
 
-        assert.equal(info, 42, "Did not pass");
+        assert.ok(Number.isInteger(info), "Did not pass");
+        console.log("createBoardTest passed")
 
     }
 
+    async userLoginTest(){
+        var response = await Query.UserLogin('username','password');
+
+        //console.log(response);
+
+        //assert.equal(response.player_id,117,"Player id is wrong");
+        //assert.equal(response.display_name, 'username', "Username is wrong");
+        //assert.equal(response.email, 'na@na.com', "User email is wrong");
+        //console.log("userLoginTest Passes")
+    }
 
     async getSpriteTest() {
-        let timeStamp = process.hrtime();
+        //let timeStamp = process.hrtime();
 
         //Query
         let spriteResults = await Query.GetSprites();
 
-        let difference = process.hrtime(timeStamp);
-        let milliseconds = (difference[0] + difference[1] / NS_PER_SEC) * 1000;
-        console.log('Sprite Test Duration(ms): ' + milliseconds);
+        //let difference = process.hrtime(timeStamp);
+        //let milliseconds = (difference[0] + difference[1] / NS_PER_SEC) * 1000;
+        //console.log('Sprite Test Duration(ms): ' + milliseconds);
 
         var correct = JSON.stringify([{"sprite_id":1,"image_url":"M_TOPHAT_BROWN"},{"sprite_id":2,"image_url":"M_TOPHAT_BLACK"},{"sprite_id":3,"image_url":"M_NEWSBOY"},{"sprite_id":4,"image_url":"M_DOCTOR_MASK"},{"sprite_id":5,"image_url":"M_DOCTOR_NOMASK"},{"sprite_id":6,"image_url":"F_NURSE"},{"sprite_id":7,"image_url":"M_DOCTOR_BLOODY"},{"sprite_id":8,"image_url":"M_TOPHAT_GLASSES"}]);
 
@@ -85,9 +97,9 @@ export class DatabaseTest {
         let results = await Query.UpdateOrInsertTile(1, 0, 0, 0,
             0, 0, 0, 0, 3);
 
-        let difference = process.hrtime(timeStamp);
-        let milliseconds = (difference[0] + difference[1] / NS_PER_SEC) * 1000;
-        console.log('Insert Tile Test Duration(ms): ' + milliseconds);
+        //let difference = process.hrtime(timeStamp);
+        //let milliseconds = (difference[0] + difference[1] / NS_PER_SEC) * 1000;
+        //console.log('Insert Tile Test Duration(ms): ' + milliseconds);
 
 
         assert.equal(results,74, "Tile is expected to be 74")
@@ -100,19 +112,19 @@ export class DatabaseTest {
         //Query
         let results = await Query.GetBoardByID(1);
 
-        let difference = process.hrtime(timeStamp);
-        let milliseconds = (difference[0] + difference[1] / NS_PER_SEC) * 1000;
-        console.log('Tiles Test Duration(ms): ' + milliseconds);
+        //let difference = process.hrtime(timeStamp);
+        //let milliseconds = (difference[0] + difference[1] / NS_PER_SEC) * 1000;
+        //console.log('Tiles Test Duration(ms): ' + milliseconds);
 
 
-        var info = results;
+        //console.log(results)
 
-        assert.equal(info.board_id, 1, "Board ID is wrong")
-        assert.equal(info.name, "Home", "Board Name is wrong")
-        assert.equal(info.creator_id, 5, "Creator ID is wrong")
-        assert.equal(info.is_deleted, 0, "Board is not deleted")
-        assert.equal(info.max_width,-1,"Max width should be -1")
-        assert.equal(info.max_height, -1, "Max height should be -1")
+        assert.equal(results.board_id, 1, "Board ID is wrong")
+        assert.equal(results.name, "Home", "Board Name is wrong")
+        assert.equal(results.creator_id, 5, "Creator ID is wrong")
+        assert.equal(results.is_deleted, 0, "Board is not deleted")
+        assert.equal(results.max_width,-1,"Max width should be -1")
+        assert.equal(results.max_height, -1, "Max height should be -1")
         console.log("getBoardInfoTest Passed");
 
     }
